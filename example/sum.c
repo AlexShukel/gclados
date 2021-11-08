e1d296ec697848af9aab568d3c46d3303283b1d1
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-11-08 17:39:36 +0200

    Created help command

---
+++ example/sum.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "../lib/ptf.h"
+
+
---
+++ lib/ptf.c
@@ -0,0 +1,7 @@
+#include "ptf.h"
+
+#include <stdio.h>
+
+void createUnit(int line, void* value) {
+    printf("Created new unit on line %d", line);
+}
