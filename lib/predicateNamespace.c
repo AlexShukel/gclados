665210e10baf7daf104b1f79329648b213a4724f
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-11-21 21:09:02 +0200

    Enabled clang-format for predicateNamespace.c file

--- lib/predicateNamespace.c
+++ lib/predicateNamespace.c
@@ -8,23 +8,21 @@
     .toEqual##name = gcladosToEqual##name, .toBeLessThan##name = gcladosToBeLessThan##name,                            \
     .toBeGreaterThan##name = gcladosToBeGreaterThan##name,                                                             \
     .toBeLessThanOrEqual##name = gcladosToBeLessThanOrEqual##name,                                                     \
-    .toBeGreaterThanOrEqual##name = gcladosToBeGreaterThanOrEqual##name,
+    .toBeGreaterThanOrEqual##name = gcladosToBeGreaterThanOrEqual##name
 
 GcladosPredicateNamespace gclados = {
-        // clang-format off
-        NUMBER_PREDICATE_ASSIGNMENTS(Char)
-        NUMBER_PREDICATE_ASSIGNMENTS(UnsignedChar)
-        NUMBER_PREDICATE_ASSIGNMENTS(Short)
-        NUMBER_PREDICATE_ASSIGNMENTS(UnsignedShort)
-        NUMBER_PREDICATE_ASSIGNMENTS(Int)
-        NUMBER_PREDICATE_ASSIGNMENTS(Unsigned)
-        NUMBER_PREDICATE_ASSIGNMENTS(Long)
-        NUMBER_PREDICATE_ASSIGNMENTS(UnsignedLong)
-        NUMBER_PREDICATE_ASSIGNMENTS(Float)
-        NUMBER_PREDICATE_ASSIGNMENTS(Double)
-        NUMBER_PREDICATE_ASSIGNMENTS(LongDouble)
+        NUMBER_PREDICATE_ASSIGNMENTS(Char),
+        NUMBER_PREDICATE_ASSIGNMENTS(UnsignedChar),
+        NUMBER_PREDICATE_ASSIGNMENTS(Short),
+        NUMBER_PREDICATE_ASSIGNMENTS(UnsignedShort),
+        NUMBER_PREDICATE_ASSIGNMENTS(Int),
+        NUMBER_PREDICATE_ASSIGNMENTS(Unsigned),
+        NUMBER_PREDICATE_ASSIGNMENTS(Long),
+        NUMBER_PREDICATE_ASSIGNMENTS(UnsignedLong),
+        NUMBER_PREDICATE_ASSIGNMENTS(Float),
+        NUMBER_PREDICATE_ASSIGNMENTS(Double),
+        NUMBER_PREDICATE_ASSIGNMENTS(LongDouble),
         .toEqualBytes = gcladosToEqualBytes,
-        // clang-format on
         .not = gcladosNot,
 };
 
